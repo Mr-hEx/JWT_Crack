e228dc934de4a7ed79003708d51a3c2a21e089d2
Mr_hEx <Mr_h3x@protonmail.com>
2019-07-12 07:14:47 -0700

    Update Crack JWT

--- JWT_crack.php
+++ JWT_crack.php
@@ -1,27 +1,20 @@
 <?php
-
-
-$handle = @fopen("test.txt", "r");
-
+$handle = @fopen("rockyou.txt", "r");
     while (! feof($handle)) {
-       $SignatureCrack  = 'UdgsiHNBvYvQaYC438a-G12ncP3NYcdqWXlrGXrpMNE'; # put here the Signature what do you want to crack it 
-	   $header = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9'; # put here the header 
-       $payload = 'eydoZXgnOicxMjU2MycsJ3Rlc3QnOic4NSd9'; # put here the payload
+       $SignatureCrack  ='9OiJd6GE55Ec-EFrEW9yEqN8pE_mT3RLkRbTcjYJhTw'; # put here the Signature what do you want to crack it
+           $header = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9'; # put here the header
+       $payload = 'eyJzb21lIjoicGF5bG9hZCJ9'; # put here the payload
        $HeaderAndPayload = "$header.$payload";
        $algo = 'sha256'; # kind of algo
-	   $keypas = fgets($handle); 
+       $keypas = fgets($handle);
        $Signature = hash_hmac($algo, $HeaderAndPayload, trim($keypas), true);
        $signatureEncoded = rtrim(strtr(base64_encode($Signature), '+/','-_'), '=');
-	   if ($signatureEncoded == $SignatureCrack) {
-           echo "Found the Key : $keypas";
-		   break;
+       if ($signatureEncoded == $SignatureCrack){
+
+               echo "\033[01;31mFound the Key : $keypas \033[0m";
+                exit();
         }
-		
-          
-  }
-    
+    }
+echo "Not found the key ):";
 fclose($handle);
-
-
-
-?> 
+?>
